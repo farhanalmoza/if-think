9fc968bc35af06801b6cba25894695ee08295d5d
Farhan <oyisamfarhan@gmail.com>
2021-04-18 23:28:21 +0700

    satu

---
+++ satu.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int umur=14;
+    
+    if (umur<=18)
+    {
+        if (umur<=10)
+        {
+            cout << "Anak-anak" << endl;
+        } else
+        {
+            cout << "Remaja" << endl;
+        }
+    } else
+    {
+        cout << "Bukan usia anak maupun remaja" << endl;
+    }
+}
+
+// source = kelas programmer
+
+// A. Anak-anak
+// B. Remaja
+// C. Bukan usia anak maupun remaja
+// D. Erro
+
+// jawaban B
